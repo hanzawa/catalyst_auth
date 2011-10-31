b886be75d49642ea469579cd8130c94c491e8a62
Kazuya Hanzawa <admin@personlink.jp>
2011-10-31 21:00:30 +0900

    Hello, World!!
    % script/myapp_server.pl

--- MyApp/lib/MyApp/Controller/Root.pm
+++ MyApp/lib/MyApp/Controller/Root.pm
@@ -30,7 +30,7 @@ sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
 
     # Hello World
-    $c->response->body( $c->welcome_message );
+    $c->response->body( "Hello, World!!" );
 }
 
 =head2 default
